2675896c9e572249fee329345214c553f9ebab94
renatocaliari <renatoac82@gmail.com>
2026-03-07 20:50:18 -0300

    chore: bump version to 0.2.0 for SSE broadcast release

--- packages/starstream/starstream/__init__.py
+++ packages/starstream/starstream/__init__.py
@@ -39,7 +39,7 @@ from .storage.base import StorageBackend
 from .storage.sqlite import SQLiteBackend
 from .helpers import throttle, debounce, RateLimiter, MessageBuilder
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = [
     "StarStreamPlugin",
     "StarStreamCore",
